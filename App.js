8394b0e0a08ce0bb41ff308d66482393505caf23
km-saifullah <kmsaifullah16@gmail.com>
2024-05-23 09:02:10 +0600

    todo app input fields and button functionality done

--- App.js
+++ App.js
@@ -1,20 +1,62 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from "react";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
-export default function App() {
+const App = () => {
+  const [listItem, setListItem] = useState("");
+
+  // handle input field
+  const handleInputField = (enteredText) => {
+    setListItem(enteredText);
+  };
+
+  // handle add list
+  const handleAddList = () => {
+    console.log(listItem);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.appContainer}>
+      <View style={styles.inputContainer}>
+        <TextInput
+          placeholder="Your List item"
+          style={styles.textInput}
+          onChangeText={handleInputField}
+        />
+        <Button title="Add Item" onPress={handleAddList} />
+      </View>
+      <View style={styles.listContainer}>
+        <Text>My List...</Text>
+      </View>
     </View>
   );
-}
+};
+
+export default App;
 
 const styles = StyleSheet.create({
-  container: {
+  appContainer: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 50,
+    paddingHorizontal: 16,
+  },
+  inputContainer: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 24,
+    borderBottomWidth: 2,
+    borderColor: "#cccccc",
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: "#cccccc",
+    borderStyle: "solid",
+    width: "75%",
+    marginRight: 8,
+    padding: 8,
+  },
+  listContainer: {
+    flex: 4,
   },
 });
