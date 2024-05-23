50d358a3bd25d5105f6b1dcec362ac7a73dbb325
km-saifullah <kmsaifullah16@gmail.com>
2024-05-23 09:21:27 +0600

    list items added to the ui

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
 const App = () => {
   const [listItem, setListItem] = useState("");
+  const [todo, setTodo] = useState([]);
 
   // handle input field
   const handleInputField = (enteredText) => {
@@ -11,21 +12,26 @@ const App = () => {
 
   // handle add list
   const handleAddList = () => {
-    console.log(listItem);
+    // setTodo([...todo, listItem]); // this is not the best way to store the data in the state variable, if the the present data is depend on next data
+    setTodo((currrentList) => [...todo, listItem]);
   };
 
   return (
     <View style={styles.appContainer}>
       <View style={styles.inputContainer}>
         <TextInput
-          placeholder="Your List item"
+          placeholder="Add your list"
           style={styles.textInput}
           onChangeText={handleInputField}
         />
         <Button title="Add Item" onPress={handleAddList} />
       </View>
       <View style={styles.listContainer}>
-        <Text>My List...</Text>
+        {todo.map((item, index) => (
+          <View key={index} style={styles.listItem}>
+            <Text style={styles.itemText}>{item}</Text>
+          </View>
+        ))}
       </View>
     </View>
   );
@@ -58,5 +64,17 @@ const styles = StyleSheet.create({
   },
   listContainer: {
     flex: 4,
+  },
+  listItem: {
+    backgroundColor: "lightblue",
+    margin: 8,
+    borderRadius: 6,
+    paddingVertical: 8,
+    paddingHorizontal: 8,
+  },
+  itemText: {
+    color: "#222222",
+    fontSize: 16,
+    fontWeight: "500",
   },
 });
